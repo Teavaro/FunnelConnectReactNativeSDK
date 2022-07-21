631a2abfa3e2732ebd38bcddbaf69aeada35fe83
Tomasz Januszek <tomasz@teavaro.com>
2022-07-21 10:43:01 +0100

    CLIEN-131 feature: Update index.js file with information

--- index.js
+++ index.js
@@ -2,8 +2,23 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+/* Docs: 
+https://reactnative.dev/docs/environment-setup
+https://reactnative.dev/docs/native-modules-android -- change to Kotlin tab on the site
+*/
+
+import {NativeModules} from 'react-native';
+
+console.log('NATIVE MODULE LOADED');
+
+const {FunnelConnectSDK} = NativeModules;
+
+// FunnelConnectSDK.something() <-- add some logging test method without params etc.
+
+module.exports = FunnelConnectSDK;
