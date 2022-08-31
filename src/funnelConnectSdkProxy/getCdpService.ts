a95e1a4315a7838d771bf36f6c020f0bb5ca1f70
Tomasz Januszek <tomasz@teavaro.com>
2022-08-31 10:50:00 +0100

    refactor: move declarations and types to dedicated files. update structure

---
+++ src/funnelConnectSdkProxy/getCdpService.ts
@@ -0,0 +1,53 @@
+export const getCdpService = (Funnelconnectreactnativesdk: any) => {
+  const cdp = () => {
+    return {
+      startCdpServiceAsync: (fcUser: FCUser | null = null): Promise<string> => {
+        return Funnelconnectreactnativesdk.startCdpServiceAsync(fcUser);
+      },
+      getUmidAsync: (): Promise<string> => {
+        return Funnelconnectreactnativesdk.getUmidAsync();
+      },
+      getUserIdAsync: (): Promise<string> => {
+        return Funnelconnectreactnativesdk.getUserIdAsync();
+      },
+      setUserAsync: (fcUser: FCUser): Promise<string> => {
+        return Funnelconnectreactnativesdk.setUserAsync(fcUser);
+      },
+      getPermissionsAsync: (): Promise<PermissionsMap> => {
+        return Funnelconnectreactnativesdk.getPermissionsAsync();
+      },
+      updatePermissions: (
+        permissions: PermissionsMap,
+        notificationsVersion: number
+      ): void => {
+        Funnelconnectreactnativesdk.updatePermissions(
+          permissions,
+          notificationsVersion
+        );
+      },
+      updatePermissionsAsync: (
+        permissions: PermissionsMap,
+        notificationsVersion: number
+      ): Promise<string> => {
+        return Funnelconnectreactnativesdk.updatePermissionsAsync(
+          permissions,
+          notificationsVersion
+        );
+      },
+      logEvent: (key: string, value: string): void => {
+        Funnelconnectreactnativesdk.logEvent(key, value);
+      },
+      logEventAsync: (key: string, value: string): Promise<string> => {
+        return Funnelconnectreactnativesdk.logEventAsync(key, value);
+      },
+      logEvents: (events: LogEventsMap): void => {
+        Funnelconnectreactnativesdk.logEvents(events);
+      },
+      logEventsAsync: (events: LogEventsMap): Promise<string> => {
+        return Funnelconnectreactnativesdk.logEventsAsync(events);
+      },
+    };
+  };
+
+  return cdp;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import { NativeModules, Platform } from 'react-native';
+import { getCdpService } from './funnelConnectSdkProxy/getCdpService';
+import { getSdkFunctions } from './funnelConnectSdkProxy/getSdkFunctions';
+import { getTrustPidService } from './funnelConnectSdkProxy/getTrustPidService';
 
 const LINKING_ERROR =
   `The package 'FunnelConnectReactNativeSDK' doesn't seem to be linked. Make sure: \n\n` +
@@ -17,153 +20,12 @@ const Funnelconnectreactnativesdk = NativeModules.FunnelConnectReactNativeSDK
       }
     );
 
-// TODO: Move types to external file when all of the shapes are decided
-type PermissionsMap = {
-  [key: string]: boolean;
-};
-
-type FCOptions = {
-  enableLogging: boolean;
-};
-
-type FCUser = {
-  userIdType: string;
-  userId: string;
-};
-
-type startTrustPidServiceOutput = {
-  atid: string;
-  mtid: string;
-};
-
-type LogEvent = {
-  key: string;
-  value: string;
-};
-
-// Top level functions
-const initializeSDK = (
-  sdkToken: string,
-  { enableLogging = true }: FCOptions
-): void => {
-  Funnelconnectreactnativesdk.initializeSDK(sdkToken, { enableLogging });
-};
-
-const onInitializeWithPromise = (): Promise<string> => {
-  return Funnelconnectreactnativesdk.onInitializeWithPromise();
-};
-
-const isInitializedWithPromise = (): Promise<boolean> => {
-  return Funnelconnectreactnativesdk.isInitializedWithPromise();
-};
-
-const clearCookies = (): void => {
-  Funnelconnectreactnativesdk.clearCookies();
-};
-
-const clearCookiesWithPromise = (): Promise<string> => {
-  return Funnelconnectreactnativesdk.clearCookiesWithPromise();
-};
-
-const clearData = (): void => {
-  Funnelconnectreactnativesdk.clearData();
-};
-
-const clearDataWithPromise = (): Promise<string> => {
-  return Funnelconnectreactnativesdk.clearDataWithPromise();
-};
-
-// CDP service functions
-const cdp = () => {
-  return {
-    startCdpServiceWithPromise: (
-      fcUser: FCUser | null = null
-    ): Promise<string> => {
-      return Funnelconnectreactnativesdk.startCdpServiceWithPromise(fcUser);
-    },
-    getUmidWithPromise: (): Promise<string> => {
-      return Funnelconnectreactnativesdk.getUmidWithPromise();
-    },
-    getUserIdWithPromise: (): Promise<string> => {
-      return Funnelconnectreactnativesdk.getUserIdWithPromise();
-    },
-    setUserWithPromise: (fcUser: FCUser): Promise<string> => {
-      return Funnelconnectreactnativesdk.setUserWithPromise(fcUser);
-    },
-    getPermissionsWithPromise: (): Promise<PermissionsMap> => {
-      return Funnelconnectreactnativesdk.getPermissionsWithPromise();
-    },
-    updatePermissions: (
-      permissions: PermissionsMap,
-      notificationsVersion: number
-    ): void => {
-      Funnelconnectreactnativesdk.updatePermissions(
-        permissions,
-        notificationsVersion
-      );
-    },
-    updatePermissionsWithPromise: (
-      permissions: PermissionsMap,
-      notificationsVersion: number
-    ): Promise<string> => {
-      return Funnelconnectreactnativesdk.updatePermissionsWithPromise(
-        permissions,
-        notificationsVersion
-      );
-    },
-    logEvent: (key: string, value: string): void => {
-      Funnelconnectreactnativesdk.logEvent(key, value);
-    },
-    logEventWithPromise: (key: string, value: string): Promise<string> => {
-      return Funnelconnectreactnativesdk.logEventWithPromise(key, value);
-    },
-    logEvents: (events: LogEvent[]): void => {
-      Funnelconnectreactnativesdk.logEvents(events);
-    },
-    logEventsWithPromise: (events: LogEvent[]): Promise<string> => {
-      return Funnelconnectreactnativesdk.logEventsWithPromise(events);
-    },
-  };
-};
-
-const trustPid = () => {
-  return {
-    startService: (isStub: boolean = false): void => {
-      Funnelconnectreactnativesdk.startTrustPidService(isStub);
-    },
-    startTrustPidServiceWithPromise: (
-      isStub: boolean = false
-    ): Promise<startTrustPidServiceOutput> => {
-      return Funnelconnectreactnativesdk.startTrustPidServiceWithPromise(
-        isStub
-      );
-    },
-    acceptConsent: (): void => {
-      Funnelconnectreactnativesdk.acceptConsent();
-    },
-    acceptConsentWithPromise: (): Promise<string> => {
-      return Funnelconnectreactnativesdk.acceptConsentWithPromise();
-    },
-    rejectConsent: (): void => {
-      Funnelconnectreactnativesdk.rejectConsent();
-    },
-    rejectConsentWithPromise: (): Promise<string> => {
-      return Funnelconnectreactnativesdk.rejectConsentWithPromise();
-    },
-    isConsentAcceptedWithPromise: (): Promise<boolean> => {
-      return Funnelconnectreactnativesdk.isConsentAcceptedWithPromise();
-    },
-  };
-};
+const coreSdkFunctions = getSdkFunctions(Funnelconnectreactnativesdk);
+const cdp = getCdpService(Funnelconnectreactnativesdk);
+const trustPid = getTrustPidService(Funnelconnectreactnativesdk);
 
 const funnelConnectSdk = {
-  initializeSDK,
-  onInitializeWithPromise,
-  isInitializedWithPromise,
-  clearCookies,
-  clearCookiesWithPromise,
-  clearData,
-  clearDataWithPromise,
+  ...coreSdkFunctions,
   cdp,
   trustPid,
 };
