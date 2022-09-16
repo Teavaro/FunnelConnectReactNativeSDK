63b06785857d3797f183f387e985158b8b420722
Tomasz Januszek <tomasz@teavaro.com>
2022-09-16 10:03:56 +0100

    feat: add exception handler wrappers to all SDK functions

--- src/funnelConnectSdkProxy/getSdkFunctions.ts
+++ src/funnelConnectSdkProxy/getSdkFunctions.ts
@@ -1,33 +1,48 @@
+import { wrapWithExceptionHandler } from './helpers/wrapWithExceptionHandler';
+import { wrapWithExceptionHandlerAsync } from './helpers/wrapWithExceptionHandlerAsync';
+
 export const getSdkFunctions = (Funnelconnectreactnativesdk: any) => {
   const initializeSDK = (
     sdkToken: string,
     { enableLogging = true }: FCOptions
   ): void => {
-    Funnelconnectreactnativesdk.initializeSDK(sdkToken, { enableLogging });
+    wrapWithExceptionHandler(
+      Funnelconnectreactnativesdk.initializeSDK,
+      sdkToken,
+      { enableLogging }
+    );
   };
 
   const onInitializeAsync = (): Promise<string> => {
-    return Funnelconnectreactnativesdk.onInitializeAsync();
+    return wrapWithExceptionHandlerAsync(
+      Funnelconnectreactnativesdk.onInitializeAsync
+    );
   };
 
   const isInitializedAsync = (): Promise<boolean> => {
-    return Funnelconnectreactnativesdk.isInitializedAsync();
+    return wrapWithExceptionHandlerAsync(
+      Funnelconnectreactnativesdk.isInitializedAsync
+    );
   };
 
   const clearCookies = (): void => {
-    Funnelconnectreactnativesdk.clearCookies();
+    wrapWithExceptionHandler(Funnelconnectreactnativesdk.clearCookies);
   };
 
   const clearCookiesAsync = (): Promise<string> => {
-    return Funnelconnectreactnativesdk.clearCookiesAsync();
+    return wrapWithExceptionHandlerAsync(
+      Funnelconnectreactnativesdk.clearCookiesAsync
+    );
   };
 
   const clearData = (): void => {
-    Funnelconnectreactnativesdk.clearData();
+    wrapWithExceptionHandler(Funnelconnectreactnativesdk.clearData);
   };
 
   const clearDataAsync = (): Promise<string> => {
-    return Funnelconnectreactnativesdk.clearDataAsync();
+    return wrapWithExceptionHandlerAsync(
+      Funnelconnectreactnativesdk.clearDataAsync
+    );
   };
 
   return {
