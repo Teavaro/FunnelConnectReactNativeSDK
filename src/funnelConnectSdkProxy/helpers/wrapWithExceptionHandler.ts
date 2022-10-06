b2aa35b8c04f742b4f3afb055663529f59b2c466
Tomasz Januszek <tomasz@teavaro.com>
2022-10-06 11:03:35 +0100

    feat: extend logging for wrappers

--- src/funnelConnectSdkProxy/helpers/wrapWithExceptionHandler.ts
+++ src/funnelConnectSdkProxy/helpers/wrapWithExceptionHandler.ts
@@ -5,8 +5,10 @@ export const wrapWithExceptionHandler = (
   try {
     wrappedFunction(...args);
   } catch (error: any) {
-    console.log(error);
-    console.log(JSON.stringify(error));
+    console.log('Sync operation error handler');
+    console.log('Error object:', error);
+    console.log('Stringified error:', JSON.stringify(error));
+    console.log('Error message:', error.message);
     throw new Error(error.message);
   }
 };
--- src/funnelConnectSdkProxy/helpers/wrapWithExceptionHandlerAsync.ts
+++ src/funnelConnectSdkProxy/helpers/wrapWithExceptionHandlerAsync.ts
@@ -5,6 +5,10 @@ export const wrapWithExceptionHandlerAsync = async (
   try {
     return await wrappedFunction(...args);
   } catch (error: any) {
+    console.log('Async operation error handler');
+    console.log('Error object:', error);
+    console.log('Stringified error:', JSON.stringify(error));
+    console.log('Error message:', error.message);
     throw new Error(error.message);
   }
 };
