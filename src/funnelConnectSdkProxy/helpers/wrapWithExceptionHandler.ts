ec341f53af2a564fef74311eb701c36d66e639ab
Tomasz Januszek <tomasz@teavaro.com>
2022-11-14 15:45:20 +0000

    refactor: remove excessive logging

--- src/funnelConnectSdkProxy/helpers/wrapWithExceptionHandler.ts
+++ src/funnelConnectSdkProxy/helpers/wrapWithExceptionHandler.ts
@@ -3,16 +3,8 @@ export const wrapWithExceptionHandler = (
   ...args: any[]
 ) => {
   try {
-    console.log(
-      `Arguments provided to the ${wrappedFunction.name} function:`,
-      ...args
-    );
     wrappedFunction(...args);
   } catch (error: any) {
-    console.log('Sync operation error handler');
-    console.log('Error object:', error);
-    console.log('Stringified error:', JSON.stringify(error));
-    console.log('Error message:', error.message);
     throw new Error(error.message);
   }
 };
--- src/funnelConnectSdkProxy/helpers/wrapWithExceptionHandlerAsync.ts
+++ src/funnelConnectSdkProxy/helpers/wrapWithExceptionHandlerAsync.ts
@@ -3,16 +3,8 @@ export const wrapWithExceptionHandlerAsync = async (
   ...args: any[]
 ) => {
   try {
-    console.log(
-      `Arguments provided to the ${wrappedFunction.name} function:`,
-      ...args
-    );
     return await wrappedFunction(...args);
   } catch (error: any) {
-    console.log('Async operation error handler');
-    console.log('Error object:', error);
-    console.log('Stringified error:', JSON.stringify(error));
-    console.log('Error message:', error.message);
     throw new Error(error.message);
   }
 };
