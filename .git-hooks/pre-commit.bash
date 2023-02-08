37848a01b8d913f1566ccc47108d3fb7b8da8b3c
Tomasz Januszek <tomasz@teavaro.com>
2023-02-08 14:54:28 +0000

    refactor: update pre-commit script; bump version

--- .git-hooks/pre-commit.bash
+++ .git-hooks/pre-commit.bash
@@ -2,7 +2,6 @@
 
 # 1. Check the remote and local (package.json) version discrepancies.
 # TODO: Make the npm version check against the diff, not a local file
-# TODO: 0.4.10 is lower than 0.4.9
 echo "➡️ Checking package version..."
 LOCAL_NPM_VERSION=$(cat "${PWD}/package.json" | grep -o '"version": "[^"]*' | grep -o '[^"]*$')
 if [[ ! $LOCAL_NPM_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
@@ -11,33 +10,49 @@ if [[ ! $LOCAL_NPM_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
 	echo "⚠️ Terminating GIT commit..."
     exit 1
 fi
+echo "✅ Package version OK."
+
 LATEST_REMOTE_GIT_TAG_VERSION=$(git -c 'versionsort.suffix=-' \
 	ls-remote --exit-code --refs --sort='version:refname' \
 	--tags https://github.com/Teavaro/FunnelConnectReactNativeSDK '*.*.*' \
 	| tail --lines=1 \
 	| cut --delimiter='/' --fields=3)
-if [[ "$LATEST_REMOTE_GIT_TAG_VERSION" > "$LOCAL_NPM_VERSION" ]] ; then
-	echo "⚠️ package.json npm version ($LOCAL_NPM_VERSION) is lower than the latest git tag version ($LATEST_REMOTE_GIT_TAG_VERSION)!"
-	echo "📢 please make sure that you have the latest changes on your local."
-	echo "⚠️ Terminating GIT commit.."
-	exit 1
-elif [[ "$LATEST_REMOTE_GIT_TAG_VERSION" == "$LOCAL_NPM_VERSION" ]] ; then
+
+# 2. Compare local and remote version
+echo "➡️ Comparing remote and local versions..."
+SPLIT_LOCAL_NPM_VERSION=(${LOCAL_NPM_VERSION//./ })
+SPLIT_LATEST_REMOTE_GIT_TAG_VERSION=(${LATEST_REMOTE_GIT_TAG_VERSION//./ })
+IS_VERSION_BUMPED=0
+for i in 0 1 2 # Indexes represent the position of the version number: 0 - Major, 1 - Minor, 2 - Patch
+do
+	if [ ${SPLIT_LATEST_REMOTE_GIT_TAG_VERSION[$i]} -gt ${SPLIT_LOCAL_NPM_VERSION[$i]} ] ; then
+		echo "⚠️ package.json npm version ($LOCAL_NPM_VERSION) is lower than the latest git tag version ($LATEST_REMOTE_GIT_TAG_VERSION) !"
+		echo "📢 please make sure that you have the latest changes on your local."
+		echo "⚠️ Terminating GIT commit..."
+		exit 1
+	elif [ ${SPLIT_LATEST_REMOTE_GIT_TAG_VERSION[$i]} -lt ${SPLIT_LOCAL_NPM_VERSION[$i]} ] ; then
+		echo "📢 Local version ($LOCAL_NPM_VERSION) is higher than the remote version ($LATEST_REMOTE_GIT_TAG_VERSION) !"
+		IS_VERSION_BUMPED=1
+		break
+	else
+		continue
+	fi
+done
+
+if [ $IS_VERSION_BUMPED -eq 0 ] ; then
+	echo "✅ Remote and local versions OK. Proceeding..."
 	exit 0
 fi
 
-# 2. With LOCAL_NPM_VERSION bigger than the remote one, check the current branch.
-# With this condition, create tag for main, reject commit for other branch.
-echo "➡️ Checking branch..."
+# 3. Prompt user to confirm tagging if the branch is main and version is bumped
+echo "➡️ Versions difference detected. Checking branch..."
 GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 if [[ "$GIT_BRANCH" == main ]]; then
 	# Show message and wait for user confirmation on tagging
-	echo "➡️ Local version detected: ${LOCAL_NPM_VERSION}."
 	echo "Please confirm the new version."
 	echo "Y - Confirmation adds a tag with the same version to publish to npm on push."
-	echo "T - Tag already present. Confirm, but do not try to create new tag."
 	echo "N - Denying terminates the commit."
-	# TODO: Remove an option to tag because the ref/tag will be at the wrong commit - the previous one before the version change in package.json, failing the publish
-	read -p "Do you confirm the new version? (Y/T/N): " ANSWER </dev/tty
+	read -p "Do you confirm the new version? (Y/N): " ANSWER </dev/tty
 	if [[ "${ANSWER}" == "Y" ]] || [[ "${ANSWER}" == "y" ]]; then
 		# Create new tag based on the package.json version
 		git tag -a $LOCAL_NPM_VERSION -m "${LOCAL_NPM_VERSION}"
@@ -50,18 +65,17 @@ if [[ "$GIT_BRANCH" == main ]]; then
 			echo -e "\033[0;31m🚫 Error code: $?"
 			exit 1
 		fi
-	# TODO: Make the tag existence validation automatic
-	elif [[ "${ANSWER}" == "T" ]] || [[ "${ANSWER}" == "t" ]]; then
-		echo "Confirmed with tag skip. Assuming the tag existence."
-		echo "✅ Please make sure to use --tags during the push to main branch. Proceeding..."
-		exit 0
 	else
-		echo -e "🚫 Confirmation not granted. You cannot bump the local version without new tag. Terminating the commit..."
+		echo -e "🚫 Confirmation not granted. You cannot bump the local version without new tag."
+		echo "⚠️ Terminating GIT commit..."
 		exit 1
 	fi
 else	# Not a main branch (feature/fix branch etc.)
-	echo "⚠️ package.json npm version ($LOCAL_NPM_VERSION) is higher than the latest git tag version ($LATEST_REMOTE_GIT_TAG_VERSION)!"
+	echo "⚠️ package.json npm version ($LOCAL_NPM_VERSION) is higher than the latest git tag version ($LATEST_REMOTE_GIT_TAG_VERSION) !"
 	echo "📢 branches other than main cannot apply version updates. Please make sure that you have the same version as remote."
 	echo "⚠️ Terminating GIT commit..."
 	exit 1
 fi
+
+echo -e "🚫 Unsupported case. Terminating GIT commit..."
+exit 1
